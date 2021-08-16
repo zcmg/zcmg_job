1824e532118408aa8ffc3056c9e6135449c1940e
zcmg <88593228+zcmg@users.noreply.github.com>
2021-08-16 18:11:34 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,19 +1,12 @@
 AddEventHandler('onClientResourceStart', function (resourceName)
     if (GetCurrentResourceName() == resourceName) then
-        TriggerEvent('chat:addSuggestion', '/codigorecompensa', 'Gerar vários códigos de recompensa', {
-            { name="tipo", help="Qual código você deseja gerar. Tipos: cash, bank, black_money, item, weapon " },
-            { name="montante/nome", help="Montante de dinheiro/Nome de item/arma" },
-            { name="quantidade", help="Quantidade de items/balas." }
-        })
-        TriggerEvent('chat:addSuggestion', '/recompensa', 'Resgatar um código de recompensa', {
-            { name="código", help="Digite o código que deseja resgatar." }
+        TriggerEvent('chat:addSuggestion', '/job', 'Permite alternar de job caso tenha permisão para tal', {
         })
     end
 end)
 
 AddEventHandler('onClientResourceStop', function(resourceName)
     if (GetCurrentResourceName() == resourceName) then
-        TriggerEvent('chat:removeSuggestion', '/codigorecompensa')
-        TriggerEvent('chat:removeSuggestion', '/recompensa')
+        TriggerEvent('chat:removeSuggestion', '/job')
     end
 end)
