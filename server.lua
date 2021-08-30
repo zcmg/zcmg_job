4e60606ff5caffe27990eb11440cd36288b78c6a
zcmg <88593228+zcmg@users.noreply.github.com>
2021-08-30 20:15:48 +0100

    Add files via upload

--- server.lua
+++ server.lua
@@ -1,55 +1,29 @@
 Config = {}
-Config.Steams = {  --Steam ids de quem podem alternar o job
-	{id ="steam:11000010064f1d9"}, -- Steam ID zcmg
-	{id ="steam:11000010067f1d8"}
-}
 
---Jobs entre qual vai alternar
-Config.Job1 = "taxi" 
-Config.Job2 = "ambulance"
+Config.Jobs = {
+	{steamid ="steam:11000010064f1d9", Job1 = "police", Job2 = "ambulance", Grade1 = 1, Grade2=3},
+}
 
 
 ESX = nil 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
---[[
-Função Job
-]]
-function job(source, args, rawCommand)
-	local _source = source
-     	local xPlayer = ESX.GetPlayerFromId(_source)
-     	local job = xPlayer.job.name
-	local grade = xPlayer.job.grade
-	
-	if job == Config.Job1 then
-		xPlayer.setJob(Config.Job2 , grade)	
-	else if job == Config.Job2  then
-		xPlayer.setJob(Config.Job1, grade)
-	end
-
-
---[[
-Mudar job
-]]--
 RegisterCommand("job", function(source, args, rawCommand)
-		local xPlayer = ESX.GetPlayerFromId(source)
-		local ver = false
-		
-		for k, v in pairs(Config.Steams) do
-			if xPlayer.identifier == v.id then
-				ver = true
-				break
-			else
-				ver = false
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local job = xPlayer.job.name
+	
+	for k, v in pairs(Config.Jobs) do
+		if xPlayer.identifier == v.steamid then
+			if job == v.Job1 then
+				xPlayer.setJob(v.Job2 , v.Grade2)	
+			elseif job == v.Job2  then
+				xPlayer.setJob(v.Job1, v.Grade1)
 			end
-		end
-		
-		if ver then
-			job(source, args, rawCommand)
 		else
-			erro(source)
+			TriggerClientEvent('chat:addMessage', source, { args = { '^7[^1Erro^7]^2', "Não tem premisões para fazer isto!"}, color = 255,255,255 })
 		end
+	end
 end, true)
-
 
